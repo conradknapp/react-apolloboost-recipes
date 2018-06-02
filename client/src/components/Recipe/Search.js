15d37e5b0fd6daeb21a93e4f6608beabac3038c1
Conrad Knapp <knappco@bc.edu>
2018-06-02 16:09:07 -0400

    Add SearchItem Component

--- client/src/components/Recipe/Search.js
+++ client/src/components/Recipe/Search.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import { ApolloConsumer } from "react-apollo";
 import { SEARCH_RECIPES } from "../../queries";
+import SearchItem from "./SearchItem";
 
 class Search extends React.Component {
   state = {
@@ -36,12 +36,7 @@ class Search extends React.Component {
             />
             <ul>
               {searchResults.map(recipe => (
-                <li key={recipe._id}>
-                  <Link to={`/recipes/${recipe._id}`}>
-                    <h4>{recipe.name}</h4>
-                  </Link>
-                  <p>{recipe.likes}</p>
-                </li>
+                <SearchItem key={recipe._id} {...recipe} />
               ))}
             </ul>
           </div>
