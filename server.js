200efd0e75cb48d9fd13b3a63bacf53966c1e708
Conrad Knapp <knappco@bc.edu>
2018-06-06 23:15:41 -0400

    Preparation for deployment

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(async (req, res, next) => {
 });
 
 // Create GraphiQL application
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+// app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 // Connect schemas with GraphQL
 app.use(
@@ -66,6 +66,14 @@ app.use(
     }
   }))
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 4444;
 
