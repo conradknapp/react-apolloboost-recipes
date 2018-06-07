6c075b1cc6b18ccfcd1ed1fcfa2dabd95a4313a0
Conrad Knapp <knappco@bc.edu>
2018-06-06 21:51:21 -0400

    Preparation for Deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 require("dotenv").config({ path: "variables.env" });
 const Recipe = require("./models/Recipe");
 const User = require("./models/User");
@@ -65,6 +66,14 @@ app.use(
     }
   }))
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 4444;
 
