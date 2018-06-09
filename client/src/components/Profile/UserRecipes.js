6d19ad41dfd1e222fb71a2eb553e6c3a078d3013
Conrad Knapp <knappco@bc.edu>
2018-06-09 04:50:11 -0400

    Adds Spinner Component When Loading

--- client/src/components/Profile/UserRecipes.js
+++ client/src/components/Profile/UserRecipes.js
@@ -8,6 +8,7 @@ import {
   GET_ALL_RECIPES,
   GET_CURRENT_USER
 } from "../../queries";
+import Spinner from "../Spinner";
 
 const handleDelete = deleteUserRecipe => {
   const confirmDelete = window.confirm(
@@ -23,7 +24,7 @@ const handleDelete = deleteUserRecipe => {
 const UserRecipes = ({ username }) => (
   <Query query={GET_USER_RECIPES} variables={{ username }}>
     {({ data, loading, error }) => {
-      if (loading) return <div>Loading</div>;
+      if (loading) return <Spinner />;
       if (error) return <div>Error</div>;
       // console.log(data);
       return (
